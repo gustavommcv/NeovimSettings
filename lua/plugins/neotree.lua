a987b501e87ebcaf1f4f1dead2e593fcc71b3fcd
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-05-07 21:41:57 -0300

    Change neotree position + add the default keybinding

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,5 +6,13 @@ return {
     "MunifTanjim/nui.nvim",
     "3rd/image.nvim",
   },
+  config = function()
+    require("neo-tree").setup({
+      window = {
+        position = "right",
+        width = 40,
+        vim.keymap.set("n", "<leader>e", "<Cmd>Neotree reveal<CR>")
+      }
+    })
+  end
 }
-

312f96de0326274982e0e45802f4f14b23bd41c7
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-05-07 21:32:20 -0300

    feat: add basic Neovim configuration
    - Add Lazy.nvim as package manager
    - Install and configure Treesitter for syntax highlighting
    - Set up Telescope for fuzzy finding
    - Include basic keymaps and settings

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  config = function()
+    require'nvim-treesitter.configs'.setup {
+      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    }
+  end
+}
+

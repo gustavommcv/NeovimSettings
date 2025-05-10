de40da4139ab66ee49b55e600f8aa84ded1c691b
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-05-09 22:25:44 -0300

    feat(treesitter): update language parser configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+			ensure_installed = "all",
 
 			highlight = {
 				enable = true,

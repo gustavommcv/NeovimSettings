4b5a202823126b6c0009c8a4be61c3f687cc524d
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-05-13 16:43:22 -0300

    refactor: add proper lsp's

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -4,8 +4,6 @@ return {
 	config = function()
 		require("alpha").setup(require("alpha.themes.dashboard").config)
 		local startify = require("alpha.themes.startify")
-		-- available: devicons, mini, default is mini
-		-- if provider not loaded and enabled is true, it will try to use another provider
 		startify.file_icons.provider = "devicons"
 	end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,45 +1,64 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "bashls", "angularls" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"bashls",
+					"angularls",
+					"cssls",
+					"emmet_language_server",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set("n", "<leader>ca", function()
-        vim.lsp.buf.code_action({
-          context = {
-            diagnostics = {},
-            only = { "source.organizeImports.ts", "source.addMissingImports.ts" },
-          },
-        })
-      end, { desc = "Code actions (auto-import)" })
+			vim.keymap.set("n", "<leader>ca", function()
+				vim.lsp.buf.code_action({
+					context = {
+						diagnostics = {},
+						only = { "source.organizeImports.ts", "source.addMissingImports.ts" },
+					},
+				})
+			end, { desc = "Code actions (auto-import)" })
 
-      local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
-    end,
-  },
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.angularls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.emmet_language_server.setup({
+				capabilities = capabilities,
+			})
+		end,
+	},
 }
